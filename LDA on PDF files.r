8beb41f2d5291014754e80e2acc145e266af7074
mstrickland256 <mstrickland256@live.com>
2017-12-03 12:15:14 +0800

    Update LDA on PDF files.r

--- LDA on PDF files.r
+++ LDA on PDF files.r
@@ -6,7 +6,6 @@ setwd("/users/shimiaoning/pdf") #Adjust this as necessary, obviously.
 
 Sys.setlocale(locale="C")
 
-
 #INPUT
 goodpath <- "/users/shimiaoning/Ukoe"
 docs <- Corpus(DirSource(goodpath), readerControl = list(reader=readPDF))
@@ -17,7 +16,6 @@ docs <- Corpus(DirSource(goodpath), readerControl = list(reader=readPDF))
 #                                                 language = "en",
 #                                                 id = "id1")
 
-
 #TEXT CLEANING
 docs <- tm_map(docs, content_transformer(tolower), lazy=TRUE)
 docs <- tm_map(docs, removePunctuation, lazy=TRUE)
@@ -25,7 +23,6 @@ docs <- tm_map(docs, removeNumbers, lazy=TRUE)
 docs <- tm_map(docs, removeWords, stopwords("english"), lazy=TRUE)
 docs <- tm_map(docs, stripWhitespace, lazy=TRUE)
 #docs <- tm_map(docs, stemDocument, lazy=TRUE)
-
 
 #DOCUMENT TERM MATRIX FORMATION
 dtm <- DocumentTermMatrix(docs)
@@ -35,7 +32,6 @@ length(freq)
 ord <- order(freq, decreasing = TRUE)
 freq[ord]
 
-
 #GIBBS PARAMETERS
 burnin <- 4000
 iter <- 300
@@ -44,18 +40,16 @@ seed <- list(2003, 5, 63, 100001, 765)
 nstart <- 5
 best <- TRUE
 
-
 #NO. OF TOPICS
 k <- 5
-
 
 #CLEARING OUT DOCUMENTS WITHOUT TERMS FROM MATRIX
 rowTotals <- apply(dtm , 1, sum)
 dtm <- dtm[rowTotals > 0, ]
 
 #LDA PROPER
-ldaOut <- LDA(dtm, k, method="Gibbs", control=list(nstart = nstart, seed = seed, best = best, burnin = burnin, iter = iter, thin = thin))
-ldaOut.terms <- as.matrix(terms(ldaOut, 6)) #2nd argument here denotes the number of terms to display per topic.
-write.csv(ldaOut.terms, file = paste("LDAGibbs", k, "TopicsTerms.csv"))
-topicProbabilities <- as.data.frame(ldaOut@gamma)
+ldaOutput <- LDA(dtm, k, method="Gibbs", control=list(nstart = nstart, seed = seed, best = best, burnin = burnin, iter = iter, thin = thin))
+ldaOutput.terms <- as.matrix(terms(ldaOutput, 6)) #2nd argument here denotes the number of terms to display per topic.
+write.csv(ldaOutput.terms, file = paste("LDAGibbs", k, "TopicsTerms.csv"))
+topicProbabilities <- as.data.frame(ldaOutput@gamma)
 write.csv(topicProbabilities, file = paste("LDAGibbs", k, "TopicProbabilities.csv"))
